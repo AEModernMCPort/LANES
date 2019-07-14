53538e33ca302b7c2e94049c94bd3f8f867c1636
Elix_x <elix.x.ggl@gmail.com>
2019-07-14 15:21:22 +0200

    Listener, physical

---
+++ src/main/java/lanes/lan/PhysicalListener.java
@@ -0,0 +1,19 @@
+package lanes.lan;
+
+import lanes.lan.mesh.Meshable;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * This interface is implemented by the engine (usually by the preprocessor(s)), and should be invoked by the implementation whenever physical changes occur.
+ *
+ * @param <M> the implementation-specific common meshable type
+ */
+public interface PhysicalListener<M extends Meshable> {
+
+	void onCreated(@NonNull M m);
+	void onDestroyed(@NonNull M m);
+
+	void onLoaded(@NonNull M m);
+	void onUnloaded(@NonNull M m);
+
+}
