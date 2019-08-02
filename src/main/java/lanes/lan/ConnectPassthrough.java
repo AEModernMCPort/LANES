2a6f024d093e9eea35574be624473f6c4c70c49e
Elix_x <elix.x.ggl@gmail.com>
2019-08-02 11:39:50 +0200

    L

--- src/main/java/lanes/lan/ConnectPassthrough.java
+++ src/main/java/lanes/lan/ConnectPassthrough.java
@@ -39,7 +39,6 @@ public interface ConnectPassthrough extends Meshable {
 	 * The limit (maximum) CP that can pass through this, or {@linkplain Optional#empty() nothing} if CP does not exist on said layer.<br>
 	 * Obviously the {@linkplain ConnectPassthrough#medium() medium} must exist in the layer as well!
 	 * @param layer layer in question
-	 * @param <L> layer type
 	 * @param <CP> CP type
 	 * @return CP limit for the layer, or {@linkplain Optional#empty()} if this does not exist on said layer
 	 */
