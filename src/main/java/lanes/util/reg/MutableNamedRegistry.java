8297e3bfca4827babd9a111d471363e8df7ebf02
Elix_x <elix.x.ggl@gmail.com>
2019-08-02 19:44:20 +0200

    Mutable registry register all & copy of

--- src/main/java/lanes/util/reg/MutableNamedRegistry.java
+++ src/main/java/lanes/util/reg/MutableNamedRegistry.java
@@ -12,19 +12,34 @@ import java.util.stream.Stream;
 
 public class MutableNamedRegistry<T extends NamedRegistry.Entry<T>> implements NamedRegistry<T> {
 
-	protected final Map<String, T> reg = new HashMap<>();
+	protected final Map<String, T> reg;
 	protected final Predicate<String> namePred;
 
-	public MutableNamedRegistry(@Nullable Predicate<String> namePred){
+	protected MutableNamedRegistry(Map<String, T> reg, Predicate<String> namePred){
+		this.reg = reg;
 		this.namePred = namePred;
 	}
 
+	public MutableNamedRegistry(@Nullable Predicate<String> namePred){
+		this(new HashMap<>(), namePred);
+	}
 	public MutableNamedRegistry(@NonNull Pattern nameRegex){
 		this(nameRegex.asMatchPredicate());
 	}
-
 	public MutableNamedRegistry(){
 		this((Predicate<String>) null);
+	}
+
+	@NonNull
+	public static <T extends NamedRegistry.Entry<T>> MutableNamedRegistry<T> copyOf(@NonNull NamedRegistry<T> reg, @Nullable Predicate<String> namePred){
+		var r = new MutableNamedRegistry<T>(namePred);
+		r.registerAll(reg);
+		return r;
+	}
+	@NonNull public static <T extends NamedRegistry.Entry<T>> MutableNamedRegistry<T> copyOf(@NonNull NamedRegistry<T> reg, @NonNull Pattern nameRegex){ return copyOf(reg, nameRegex.asMatchPredicate()); }
+	@NonNull public static <T extends NamedRegistry.Entry<T>> MutableNamedRegistry<T> copyOf(@NonNull NamedRegistry<T> reg){ return copyOf(reg, (Predicate<String>) null); }
+	@NonNull public static <T extends NamedRegistry.Entry<T>> MutableNamedRegistry<T> copyOfWithNamePredicate(@NonNull MutableNamedRegistry<T> reg){
+		return new MutableNamedRegistry<>(new HashMap<>(reg.reg), reg.namePred);
 	}
 
 	@Override
@@ -43,6 +58,9 @@ public class MutableNamedRegistry<T extends NamedRegistry.Entry<T>> implements N
 		if(namePred != null && !namePred.test(t.getName())) throw new IllegalArgumentException(String.format("Cannot register %s - name (%s) does not validate registry's naming convention.", t, t.getName()));
 		reg.put(t.getName(), t);
 	}
+
+	public void registerAll(@NonNull Stream<T> ts){ ts.forEach(this::register); }
+	public void registerAll(@NonNull NamedRegistry<T> reg){ registerAll(reg.entries()); }
 
 	@NonNull
 	public ImmutableNamedRegistry<T> finaliz(){
