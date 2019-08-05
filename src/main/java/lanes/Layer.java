257f3a38b3538dc6c44e2d889d9612e4ce46baa5
Elix_x <elix.x.ggl@gmail.com>
2019-08-06 00:34:38 +0200

    Layer instances

--- src/main/java/lanes/Layer.java
+++ src/main/java/lanes/Layer.java
@@ -1,13 +1,17 @@
 package lanes;
 
+import lanes.lan.mesh.Meshable;
+import lanes.lan.mesh.PhysicalMesher;
 import lanes.util.reg.NamedRegistry;
 import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.function.Function;
 
 public abstract class Layer<CP extends ConnectParam<CP>, L extends Layer<CP, L>> implements NamedRegistry.Entry<L> {
 
 	public final String name;
 
-	public Layer(String name){
+	public Layer(@NonNull String name){
 		this.name = name;
 	}
 
@@ -15,6 +19,45 @@ public abstract class Layer<CP extends ConnectParam<CP>, L extends Layer<CP, L>>
 	@Override
 	public String getName(){
 		return name;
+	}
+
+	public <M extends Meshable> InstanceBuilder<M> newInstance(){ return new InstanceBuilder<>(); }
+	public class Instance<M extends Meshable> {
+
+		private PhysicalMesher<CP, L, M> physicalMesher;
+
+		protected Instance(){}
+
+		@NonNull
+		public L getLayer(){
+			return (L) Layer.this;
+		}
+
+		@NonNull
+		public PhysicalMesher<CP, L, M> getPhysicalMesher(){
+			return physicalMesher;
+		}
+
+	}
+	public class InstanceBuilder<M extends Meshable> {
+
+		protected Function<Instance<M>, PhysicalMesher<CP, L, M>> physicalMesher;
+
+		protected InstanceBuilder(){}
+
+		@NonNull
+		public InstanceBuilder<M> setPhysicalMesher(@NonNull Function<Instance<M>, PhysicalMesher<CP, L, M>> physicalMesher){
+			this.physicalMesher = physicalMesher;
+			return this;
+		}
+
+		@NonNull
+		public Instance<M> build(){
+			var instance = new Instance<M>();
+			instance.physicalMesher = physicalMesher.apply(instance);
+			return instance;
+		}
+
 	}
 
 	//TODO Access to (0,1) constants, in a more proper-"static" way
